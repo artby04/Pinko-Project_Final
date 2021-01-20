a8c0f07cb571f1b748c51e2e1c9f46f2fbe4fd32
artby04 <52202830+artby04@users.noreply.github.com>
2021-01-20 22:48:36 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -783,7 +783,26 @@ let burger = document.getElementById('burger'),
 } 
 )
 
+document.querySelectorAll('a[href^="#"').forEach(link => {
 
+  link.addEventListener('click', function(e) {
+      e.preventDefault();
+
+      let href = this.getAttribute('href').substring(1);
+
+      const scrollTarget = document.getElementById(href);
+
+      const topOffset = document.querySelector('.scrollto').offsetHeight +95;
+      // const topOffset = 0; // если не нужен отступ сверху 
+      const elementPosition = scrollTarget.getBoundingClientRect().top;
+      const offsetPosition = elementPosition - topOffset;
+
+      window.scrollBy({
+          top: offsetPosition,
+          behavior: 'smooth'
+      });
+  });
+});
 
 
 
